884b17aed227006b4eaaaa63b73a968497590d02
Chávez Morelos Noé Baldemar <noe_fam_chavez@yahoo.com.mx>
2021-06-04 22:46:52 -0500

    Creación de rama fundamientos de Firabase y limpieza de la rama maestra, para empezar a realizar el blog.

--- app/src/main/java/mx/com/disoftware/blogapp/MainActivity.kt
+++ app/src/main/java/mx/com/disoftware/blogapp/MainActivity.kt
@@ -19,119 +19,11 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var imageView: ImageView
-    private lateinit var btnTakePicture: Button
-
-    private var resultLauncher = this.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        if (result.resultCode == Activity.RESULT_OK) {
-            val data = result.data
-            val imageBitmap = data?.extras?.get("data") as Bitmap
-            imageView.setImageBitmap(imageBitmap)
-            uploadPicture(imageBitmap);
-        }
-    }
-
     private var REQUEST_IMAGE_CAPTURE = 1;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        imageView = findViewById(R.id.imageView)
-        btnTakePicture = findViewById(R.id.btn_take_picture)
-
-        // Creando instancia a firebase
-        val db = FirebaseFirestore.getInstance()
-
-        /* -------------- Información cada vez que se inicia la app ---------------------- */
-
-        // Consultando informacion a Firebease
-        db.collection("ciudades")
-            .document("LA")
-            .get()
-            .addOnSuccessListener { document ->
-                // se puede obtener tod0 el objeto meidante una conversion
-                val ciudad = document.toObject(Ciudad::class.java)
-                Log.d("Firebase", "DocumentSnapshot data: ${ciudad.toString()}")
-                Log.d("Firebase", "DocumentSnapshot data: ${document.data}")
-        }.addOnFailureListener { error ->
-                Log.d("Firebase", "DocumentSnapshot data: ${error.toString()}")
-        }
-
-        // Escribiendo datos a Firebase
-        db.collection("ciudades")
-            .document("CDMX")
-            .set(Ciudad(2500000, "Orange"))
-            .addOnSuccessListener {
-                Log.d("Firebase", "Ciudad guardada de manera correcta")
-            }.addOnFailureListener { error ->
-                Log.d("Firebase", "Error ${error.toString()}")
-            }
-
-        /* -------------- Información en tiempo real que se inicia la app ---------------------- */
-        db.collection("ciudades")
-            .document("LA")
-            .addSnapshotListener { value, error ->
-                value?.let { document ->
-                    // se puede obtener tod0 el objeto meidante una conversion
-                    val ciudad = document.toObject(Ciudad::class.java)
-                    Log.d("Firebase", "DocumentSnapshot data mediante objeto: ${ciudad.toString()}")
-                    Log.d("Firebase", "DocumentSnapshot data: ${document.data}")
-                }
-                error.let {
-                    Log.d("Firebase", "Ha ocurrido un error: ${it.toString()}")
-                }
-            }
-
-        btnTakePicture.setOnClickListener {
-            //dispatchTakePictureIntent()
-            openSomeActivityForResult()
-        }
-
-    }
-
-    fun openSomeActivityForResult() {
-        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        resultLauncher.launch(takePictureIntent)
-    }
-
-    // Para subir la foto a firestore de Firebase
-    fun uploadPicture(bitmap: Bitmap) {
-        // Obteniendo una referencia al almacenamiento Storage de Firebase
-        val storageRef = FirebaseStorage.getInstance().reference
-        // Nombre con el que se guardará la imagen
-        var imageRef = storageRef.child("${UUID.randomUUID()}.jpg") // UUID crea un nombre unico
-        // Guardar la imagen dentro de una carpeta
-        imageRef = storageRef.child("image/${UUID.randomUUID()}.jpg")
-        // Comprimir imagen
-        val baos = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
-        val data = baos.toByteArray()
-        val uploadTask = imageRef.putBytes(data)
-
-        uploadTask.continueWithTask { task ->
-            // Si ocurre un error al regresar la promesa
-            if(!task.isSuccessful) {
-                task.exception?.let { exception ->
-                    throw exception
-                }
-            }
-            imageRef.downloadUrl
-        }.addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                val downLoadUrl = task.result.toString()
-                // Agregando una imagen a la base de FireStore a un dato ya existente
-                FirebaseFirestore
-                    .getInstance()
-                    .collection("ciudades")
-                    .document("LA")
-                    .update(mapOf("imageUrl" to downLoadUrl))
-                Log.d("Storage", "uploadPrictureUrl: $downLoadUrl")
-            }
-        }
-
     }
 
 }
-
-data class Ciudad(val population: Int = 0, val color:String = "", val downLoadUrl:String = "")
